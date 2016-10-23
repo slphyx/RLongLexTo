2f0c6a9c0253c62921c9cfb37c37d5f202222127
slphyx <sakngoi@hotmail.com>
2016-10-23 23:16:08 +0700

    edit the dict file name

--- R/RLongLexToF.R
+++ R/RLongLexToF.R
@@ -2,7 +2,7 @@
 # read the input from inputfilename and write the out to outputfilename
 
 #default dictionary file is lexitron.txt
-PATH2DICT<-paste0(system.file(package='RLongLexTo'),"/java/lexitron.txts")
+PATH2DICT<-paste0(system.file(package='RLongLexTo'),"/java/lexitron.txt")
 
 RLongLexToF<-function(inputfilename,outputfilename,path2dict=NULL){
   if(is.null(path2dict)){
